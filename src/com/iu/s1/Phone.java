e2f01d5ec12618f55d47095ec1e16d474772a0ca
jazzhong1 <jazzhong1@naver.com>
2018-02-22 14:34:50 +0900

    ex_2

--- src/com/iu/s1/Phone.java
+++
@@ -1,5 +0,0 @@
-package com.iu.s1;
-
-public class Phone {
-
-}
---
+++ src/com/iu/s4/Tv.java
@@ -0,0 +1,21 @@
+package com.iu.s4;
+
+public class Tv extends Product {
+	private int size;
+	private String brind;
+	
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getBrind() {
+		return brind;
+	}
+	public void setBrind(String brind) {
+		this.brind = brind;
+	}
+
+}
